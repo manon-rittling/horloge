efbcb2228fdcbe2794324461e5d678cb6bceb73f
manon-rittling <manon.rittling@laplateforme.io>
2023-12-04 10:41:27 +0100

    horloge fini pour de bon

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import time
 import os
 
 
-heure = (23,59,57)
-alarme=(0,0,2)
+heure = (16,59,57)
+alarme=(17,0,0)
 
-def afficher_heure(heure):
+def afficher_heure(heure): 
     
     nouvelle_heure = list(heure) #Vous convertissez le tuple heure en liste nouvelle_heure pour permettre la modification des valeurs.
     h, minute, seconde = nouvelle_heure  #Vous extrayez les heures, minutes et secondes de la liste pour pouvoir les manipuler individuellement.
 
-    print(f'{h:02d}:{minute:02d}:{seconde:02d}')
+    
 
     while True:
         
@@ -30,28 +30,30 @@ def afficher_heure(heure):
             
             am_pm(h, minute, seconde, format_choisi)
             
-format_choisi = input("Choisissez le format (24h ou am/pm): ")
+format_choisi = input("Choisissez le format (24h ou am/pm): ") #Vous demandez à l'utilisateur de choisir entre le format 24h et am/pm et stockez la réponse dans la variable format_choisi
 
             
-def regler_alarme(heure, alarme):
+def regler_alarme(heure, alarme): #Définition de la fonction regler_alarme qui imprime un message si l'alarme correspond à l'heure.
         if alarme == heure :
             print("C'est l'heure du réveil")
 
-def am_pm(h, minute, seconde, format_choisi):
+def am_pm(h, minute, seconde, format_choisi): #Début de la définition de la fonction am_pm pour afficher l'heure au format 24h ou am/pm.
     if format_choisi.lower() == "24h":
-        print(f"{h:02d}:{minute:02d}:{seconde:02d}")
-    elif format_choisi.lower() == "am/pm":
-        suffixe = "AM"
+        heure_test = f"{h:02d}:{minute:02d}:{seconde:02d}"
+        print(heure_test, end="\r")
+    elif format_choisi.lower() == "am/pm": # Si le format choisi est "am/pm"
+        suffixe = "AM" # Si le format choisi est "am/pm"
         if h >= 12:
-            suffixe = 'PM'
+            suffixe = 'PM'  # Si l'heure est supérieure à 12, ajuste l'heure en format 12 heures
         if h > 12:
             h -= 12
-        elif h == 0:
+        elif h == 0:  # Si l'heure est 0, ajuste l'heure à 12 pour le format 12 heures
             h = 12
 
-        print(f'{h:02d}:{minute:02d}:{seconde:02d} {suffixe}')
+        heure_test=f'{h:02d}:{minute:02d}:{seconde:02d} {suffixe}'
+        print(heure_test, end="\r")
     else:
-        print("Format non reconnu. Veuillez choisir entre '24h' ou 'am/pm'.")
+        print("Format non reconnu. Veuillez choisir entre '24h' ou 'am/pm':")
 
 afficher_heure(heure)
 
